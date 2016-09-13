a2d9203b7cb927b9011c64d62efd06b543e46eb6
Samuel Walladge <samuel@swalladge.id.au>
2016-09-13 15:14:34 +0930

    initial commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# script to set up the environment ready for running
+
+# create virtual env
+pyvenv env
+source env/bin/activate
+
+# install requirements
+pip install -r requirements.txt
+
+# install adafruit library manually
+# (didn't work with pip :shrug:)
+git clone https://github.com/adafruit/Adafruit_Python_MCP9808.git
+cd Adafruit_Python_MCP9808
+python setup.py install
+cd ..
+
+# anything else needed here...
