d17f931e9ab62fb986d37c6fa65c6795497780c0
Phil Davis <phil.davis@inf.org>
2016-09-30 16:20:59 +0930

    Current temperature tests
    Add basic tests for existence and format of current temperature endpoint.
    Note: once we can generate known test data, then we can test the calculation values.

--- api_tests_spec.js
+++ api_tests_spec.js
@@ -4,6 +4,7 @@ frisby.create('Temperature sensor info test')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .expectJSON({
+    success: true,
     data: {
       live: false,
       log_interval: 5,
@@ -15,5 +16,110 @@ frisby.create('Temperature sensor info test')
     data: {
       timezone: Number
     }
+  })
+.toss();
+
+frisby.create('Current temperature test')
+  .get('http://localhost:8888/api/temperature/current')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      unit: String,
+      temperature: Number,
+      timestamp: Number
+    }
+  })
+.toss();
+
+frisby.create('Current temperature C test')
+  .get('http://localhost:8888/api/temperature/current?unit=C')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      unit: String,
+      temperature: Number,
+      timestamp: Number
+    }
+  })
+.toss();
+
+frisby.create('Current temperature F test')
+  .get('http://localhost:8888/api/temperature/current?unit=F')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      unit: "F"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      unit: String,
+      temperature: Number,
+      timestamp: Number
+    }
+  })
+.toss();
+
+frisby.create('Current temperature K test')
+  .get('http://localhost:8888/api/temperature/current?unit=K')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      unit: "K"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      unit: String,
+      temperature: Number,
+      timestamp: Number
+    }
+  })
+.toss();
+
+frisby.create('Current temperature R test')
+  .get('http://localhost:8888/api/temperature/current?unit=R')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      unit: "R"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      unit: String,
+      temperature: Number,
+      timestamp: Number
+    }
+  })
+.toss();
+
+frisby.create('Current temperature invalid unit test')
+  .get('http://localhost:8888/api/temperature/current?unit=X')
+  .expectStatus(400)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    reason: "invalid unit parameter",
+    success: false
   })
 .toss();
