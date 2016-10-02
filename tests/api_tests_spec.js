a75f369a96cc812942c0f2f33a780347d88540f7
Phil Davis <phil.davis@inf.org>
2016-10-02 23:16:46 +0930

    Add tests for from-to for max

--- tests/api_tests_spec.js
+++ tests/api_tests_spec.js
@@ -1071,6 +1071,220 @@ frisby.create('Temperature max invalid unit test')
   })
 .toss();
 
+// Get the max specifying "from" and "to" that match exact data points
+frisby.create('Temperature max from-to match test')
+  .get('http://localhost:8888/api/temperature/max?from=1451611700&to=1451612600')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      max: {
+        temperature: 34.5,
+        timestamp: 1451611900
+	  },
+      count: 10,
+      from: 1451611700,
+      to: 1451612600,
+      lower: 1451611700,
+      upper: 1451612600,
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      max: {
+        temperature: Number,
+        timestamp: Number
+	  },
+      count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+    }
+  })
+.toss();
+
+// Get the max specifying "from" and "to" that do not match exact data points
+frisby.create('Temperature max from-to non-match test')
+  .get('http://localhost:8888/api/temperature/max?from=1451611650&to=1451612650')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      max: {
+        temperature: 34.5,
+        timestamp: 1451611900
+	  },
+      count: 10,
+      from: 1451611650,
+      to: 1451612650,
+      lower: 1451611700,
+      upper: 1451612600,
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      max: {
+        temperature: Number,
+        timestamp: Number
+	  },
+      count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+    }
+  })
+.toss();
+
+// Get the max specifying just "from" that matches an exact data point
+frisby.create('Temperature max from match test')
+  .get('http://localhost:8888/api/temperature/max?from=1451613000')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      max: {
+        temperature: 8.4,
+        timestamp: 1451614900
+	  },
+      count: 20,
+      from: 1451613000,
+      lower: 1451613000,
+      upper: 1451614900,
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      max: {
+        temperature: Number,
+        timestamp: Number
+	  },
+      count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+    }
+  })
+.toss();
+
+// Get the max specifying just "from" that does not match an exact data point
+frisby.create('Temperature max from non-match test')
+  .get('http://localhost:8888/api/temperature/max?from=1451612990')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      max: {
+        temperature: 8.4,
+        timestamp: 1451614900
+	  },
+      count: 20,
+      from: 1451612990,
+      lower: 1451613000,
+      upper: 1451614900,
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      max: {
+        temperature: Number,
+        timestamp: Number
+	  },
+      count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+    }
+  })
+.toss();
+
+// Get the max specifying just "to" that matches an exact data point
+frisby.create('Temperature max to match test')
+  .get('http://localhost:8888/api/temperature/max?to=1451610700')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      max: {
+        temperature: 25.6,
+        timestamp: 1451610500
+	  },
+      count: 8,
+      from: 0,
+      to: 1451610700,
+      lower: 1451610000,
+      upper: 1451610700,
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      max: {
+        temperature: Number,
+        timestamp: Number
+	  },
+      count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+    }
+  })
+.toss();
+
+// Get the max specifying just "to" that does not match an exact data point
+frisby.create('Temperature max to non-match test')
+  .get('http://localhost:8888/api/temperature/max?to=1451610735')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      max: {
+        temperature: 25.6,
+        timestamp: 1451610500
+	  },
+      count: 8,
+      from: 0,
+      to: 1451610735,
+      lower: 1451610000,
+      upper: 1451610700,
+      unit: "C"
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      max: {
+        temperature: Number,
+        timestamp: Number
+	  },
+      count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+    }
+  })
+.toss();
+
 // Test that the min temperature endpoint exists
 // and returns reasonable data.
 
