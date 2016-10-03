ca3719770e2814fea71eb308123b278112c09269
Samuel Walladge <swalladge@users.noreply.github.com>
2016-10-03 12:22:17 +1030

    add grunt tests for webapp (#49)
    * add grunt tests for webapp
    
    * fix js errors
    
    * fix html validation errors
    
    * fix bootlint errors, we want to use a select element, so ignore E006
    
    * fix csslint warnings

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  delete process.env._JAVA_OPTIONS;
+
+  grunt.initConfig({
+    htmllint: {
+      all: {
+        src: ['webapp/*.html'],
+        options: {
+          ignore: /Section lacks heading./
+        }
+      }
+    },
+    jshint: {
+      all: {
+        src: ['Gruntfile.js', 'webapp/js/**/*.js', 'tests/*.js']
+      }
+    },
+    csslint: {
+      all: {
+        src: ['webapp/css/**/*.css']
+      }
+    },
+    bootlint: {
+      options: {
+        stoponerror: true,
+        relaxerror: ['E006']
+      },
+      files: ['webapp/*.html']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-html');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-bootlint');
+
+  grunt.registerTask('default', ['htmllint', 'jshint', 'csslint', 'bootlint']);
+
+};
