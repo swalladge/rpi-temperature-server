cce76b0b8c29684da5ec528dae51efb2f17191b7
Phil Davis <phil.davis@inf.org>
2016-10-03 15:22:28 +0930

    array no data points

--- tests/api_temperature_array_tests_spec.js
+++ tests/api_temperature_array_tests_spec.js
@@ -473,3 +473,36 @@ frisby.create('Temperature array to non-match test')
     }
   })
 .toss();
+
+// Get an array of temperatures "from" and "to" that contains no data points
+frisby.create('Temperature array from-to null test')
+  .get('http://localhost:8888/api/temperature?from=1451612702&to=1451612755')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      count: 0,
+      full_count: 0,
+      from: 1451612702,
+      to: 1451612755,
+      lower: null,
+      upper: null,
+      unit: "C",
+      temperature_array: []
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      count: Number,
+      full_count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+      temperature_array: Array
+    }
+  })
+.toss();
+
