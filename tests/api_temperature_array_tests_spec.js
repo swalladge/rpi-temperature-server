a763463c15fe9019d93529022330fac1dff56ed4
Phil Davis <phil.davis@inf.org>
2016-10-03 14:23:56 +0930

    Temperature array from-to test

--- tests/api_temperature_array_tests_spec.js
+++ tests/api_temperature_array_tests_spec.js
@@ -157,3 +157,56 @@ frisby.create('Temperature array invalid unit test')
     success: false
   })
 .toss();
+
+// Get an array of temperatures "from" and "to" that match exact data points
+frisby.create('Temperature array from-to match test')
+  .get('http://localhost:8888/api/temperature?from=1451612700&to=1451613100')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    success: true,
+    data: {
+      count: 5,
+      full_count: 5,
+      from: 1451612700,
+      to: 1451613100,
+      lower: 1451612700,
+      upper: 1451613100,
+      unit: "C"
+      temperature_array: {[
+        {
+          temperature: 5.5,
+          timestamp: 1451612700
+        },
+        {
+          temperature: 2.2,
+          timestamp: 1451612800
+        },
+        {
+          temperature: 0.0,
+          timestamp: 1451612900
+        },
+        {
+          temperature: -0.1,
+          timestamp: 1451613000
+        },
+        {
+          temperature: -1.0,
+          timestamp: 1451613100
+        },
+	  ]},
+    }
+  })
+  .expectJSONTypes({
+    data: {
+      count: Number,
+      full_count: Number,
+      from: Number,
+      to: Number,
+      lower: Number,
+      upper: Number,
+      unit: String,
+      temperature_array: Array
+    }
+  })
+.toss();
